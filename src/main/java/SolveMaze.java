be6cecd74060cd9418bdd996b65fd760d7d43e0a
xingda <xingdal2@illinois.edu>
2018-09-12 16:19:19 -0500

    finished

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,7 +35,29 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft();
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    if (maze.canMove()) {
+                        maze.move();
+                    } else {
+                        maze.turnRight();
+                        if (maze.canMove()) {
+                            maze.move();
+                        } else {
+                            maze.turnRight();
+                            maze.move();
+                        }
+                    }
+                }
+
+            }
         }
+
+
 
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
